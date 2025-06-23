51021883837619e1e8cf318605a597a0ddeb48a1
xinyihl <1012737146@qq.com>
2025-06-23 18:40:38 +0800

    代码清理

--- src/main/java/com/xinyihl/whimcraft/WhimCraft.java
+++ src/main/java/com/xinyihl/whimcraft/WhimCraft.java
@@ -7,16 +7,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION, dependencies =
-        "required-after:mixinbooter;"
-        + "required-after:configanytime;"
-//        + "required-after:jei;"
-//        + "required-after:modularmachinery;"
-//        + "required-after:thaumcraft;"
-//        + "required-after:thaumicenergistics;"
-//        + "required-after:appliedenergistics2;"
-        //+ "required-after:gugu-utils;"
-)
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION, dependencies = "required-after:mixinbooter;required-after:configanytime;")
 public class WhimCraft {
     @SidedProxy(clientSide = "com.xinyihl.whimcraft.common.proxy.ClientProxy", serverSide = "com.xinyihl.whimcraft.common.proxy.CommonProxy")
     public static CommonProxy PROXY;
