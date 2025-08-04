b47ee327e56caf4f97837a42262a167112c12cbb
xinyihl <1012737146@qq.com>
2025-08-04 14:16:20 +0800

    fixed: 模块化相关方块无法跟随控制器变色

--- src/main/java/com/xinyihl/whimcraft/common/items/MyItemBlock.java
+++ src/main/java/com/xinyihl/whimcraft/common/items/MyItemBlock.java
@@ -1,16 +1,23 @@
 package com.xinyihl.whimcraft.common.items;
 
 import com.xinyihl.whimcraft.common.init.IB;
+import hellfirepvp.modularmachinery.common.data.Config;
+import hellfirepvp.modularmachinery.common.item.ItemBlockMachineComponent;
 import net.minecraft.block.Block;
-import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 
 import java.util.Objects;
 
-public class MyItemBlock extends ItemBlock {
+public class MyItemBlock extends ItemBlockMachineComponent {
     public MyItemBlock(Block block) {
         super(block);
         this.setRegistryName(Objects.requireNonNull(block.getRegistryName()));
         this.setTranslationKey(block.getTranslationKey());
         IB.items.add(this);
+    }
+
+    @Override
+    public int getColorFromItemstack(ItemStack stack, int tintIndex) {
+        return stack.isEmpty() ? 0 : Config.machineColor;
     }
 }
--- src/main/java/com/xinyihl/whimcraft/common/proxy/ClientProxy.java
+++ src/main/java/com/xinyihl/whimcraft/common/proxy/ClientProxy.java
@@ -1,7 +1,17 @@
 package com.xinyihl.whimcraft.common.proxy;
 
 import com.xinyihl.whimcraft.common.event.GetItemKeyHandler;
+import hellfirepvp.modularmachinery.common.block.BlockDynamicColor;
+import hellfirepvp.modularmachinery.common.item.ItemDynamicColor;
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.color.BlockColors;
+import net.minecraft.client.renderer.color.ItemColors;
+import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
+
+import static com.xinyihl.whimcraft.common.init.IB.blocks;
+import static com.xinyihl.whimcraft.common.init.IB.items;
 
 @SuppressWarnings("unused")
 public class ClientProxy extends CommonProxy {
@@ -15,5 +25,19 @@ public class ClientProxy extends CommonProxy {
     public void init() {
         super.init();
         MinecraftForge.EVENT_BUS.register(new GetItemKeyHandler());
+        BlockColors blockColors = Minecraft.getMinecraft().getBlockColors();
+        for (Block block : blocks.values()) {
+            if (block instanceof BlockDynamicColor) {
+                BlockDynamicColor blockDynamicColor = (BlockDynamicColor) block;
+                blockColors.registerBlockColorHandler(blockDynamicColor::getColorMultiplier, block);
+            }
+        }
+        ItemColors itemColors = Minecraft.getMinecraft().getItemColors();
+        for (Item item : items) {
+            if (item instanceof ItemDynamicColor) {
+                ItemDynamicColor itemDynamicColor = (ItemDynamicColor) item;
+                itemColors.registerItemColorHandler(itemDynamicColor::getColorFromItemstack, item);
+            }
+        }
     }
 }
